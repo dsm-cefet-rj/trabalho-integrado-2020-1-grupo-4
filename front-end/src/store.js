f1966451bfd860891e7e065dd09bbc131fbad6a8
Zirondi <zirondil@gmail.com>
2020-11-23 19:46:05 -0300

    Editando o store.js

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,8 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import projetosReducer from './projetos/ProjetosSlice'
 
 export const store = configureStore({
-    reducer: {
-      projetos: projetosReducer
-    }
 })
