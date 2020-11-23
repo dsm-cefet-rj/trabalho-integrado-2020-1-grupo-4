995bc888b401f1cc7dc8a699b24aa0c25af6d702
Zirondi <zirondil@gmail.com>
2020-11-23 14:54:34 -0300

    De home para telas

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -7,15 +7,16 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Home from './home/Home';
+import Telas from './telas/Telas';
 import Template from './template/Template';
+import NotFound from './notfound/NotFound';
 import NotFound from './notfound/NotFound';
 
 export default function Routes() {
     return (
         <BrowserRouter>
           <Switch>
-            <Route path='/' exact component={ () => <Home />} />
+            <Route path='/' exact component={ () => <Telas />} />
             <Route path='/Template' component={ () => <Template />} />
             <Route path='/notfound' component={() => <NotFound />} />
           </Switch>
---
+++ front-end/src/telas/Telas.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Telas.css";
+
+export default function Telas() {
+    return (
+        <div className="Telas">
+            <h1>Isto é o conjunto de telas prontas!</h1>
+                <ol>
+                    <li><Link to="/template">Template</Link></li>
+                    <li><Link to="/notfound">NotFound</Link></li>
+                </ol>
+        </div>
+    );
+}
