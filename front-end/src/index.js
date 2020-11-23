41969762d24591c682bd1bd20ba9400d052016c3
Zirondi <zirondil@gmail.com>
2020-11-22 23:25:31 -0300

    Estrutura

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,42 +1,13 @@
-import Amplify from 'aws-amplify';
-import config from './config';
-import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-Amplify.configure({
-    Auth: {
-        mandatorySignIn: true,
-        region: config.cognito.REGION,
-        userPoolId: config.cognito.USER_POOL_ID,
-        identityPoolId: config.cognito.IDENTITY_POOL_ID,
-        userPoolWebClientId: config.cognito.APP_CLIENT_ID
-    },
-    Storage: {
-        region: config.s3.REGION,
-        bucket: config.s3.BUCKET,
-        identityPoolId: config.cognito.IDENTITY_POOL_ID
-    },
-    API: {
-        endpoints: [
-            {
-                name: "notes",
-                endpoint: config.apiGateway.URL,
-                region: config.apiGateway.REGION
-            },
-        ]
-    }
-});
 
 ReactDOM.render(
-    <Router>
+    <Router.StrictMode>
       <App />
-    </Router>,
+    </Router.StrictMode>,
     document.getElementById('root')
   );
 
-
-serviceWorker.unregister();
