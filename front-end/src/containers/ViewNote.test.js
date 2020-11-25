7223af746009bedb410a18a16aa38b4f3cba8c75
Vinicius Jesus <vinicius.jesus@ouronegro.com.br>
2020-11-25 18:25:20 -0300

    Teste de aceitação - Notas

---
+++ front-end/src/containers/ViewNote.test.js
@@ -0,0 +1,7 @@
+//Visualizar Nota
+
+test('Visualizar uma nota existente', () => {throw 'Not implemented yet'});
+
+//test('Visualizar uma nota inexistente', () => {throw 'Not implemented yet'});
+
+//test('Visualizar uma nota excluído previamente', () => {throw 'Not implemented yet'});
