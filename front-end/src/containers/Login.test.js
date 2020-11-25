87b1a07b932c3231160dc96636f8f6023dfc3819
Denis Crispe Silva <deniscrispe@gmail.com>
2020-11-24 22:47:40 -0300

    Testes de defeito

---
+++ front-end/src/containers/Login.test.js
@@ -0,0 +1,15 @@
+//####### CAMPO EMAIL ################################
+
+test('Email válido, cadastrado', () => {throw 'Not implemented yet'});
+
+test('Email inválido, não cadastrado', () => {throw 'Not implemented yet'});
+
+test('Email vazio', () => {throw 'Not implemented yet'});
+
+//####### CAMPO SENHA ################################
+
+test('Senha válida, correspondente ao email', () => {throw 'Not implemented yet'});
+
+test('Senha inválida, incorreta', () => {throw 'Not implemented yet'});
+
+test('Senha vazia', () => {throw 'Not implemented yet'});
---
+++ front-end/src/containers/Signup.test.js
@@ -0,0 +1,63 @@
+//####### CAMPO NOME ################################
+
+test('Nome vazio', () => {throw 'Not implemented yet'});
+
+test('Nome limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Nome válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO EMAIL ################################
+
+test('Email vazio', () => {throw 'Not implemented yet'});
+
+test('Email limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Email válido', () => {throw 'Not implemented yet'});
+
+test('Email limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Email limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Email limite superior inválido', () => {throw 'Not implemented yet'});
+
+test('Email inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO CONFIRMACAO EMAIL ################################
+
+test('Confirmação de Email inválido, diferente do email anterior', () => {throw 'Not implemented yet'});
+
+test('Confirmação de Email válida', () => {throw 'Not implemented yet'});
+
+//####### CAMPO SENHA ################################
+
+test('Senha vazia', () => {throw 'Not implemented yet'});
+
+test('Senha limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Senha válida', () => {throw 'Not implemented yet'});
+
+test('Senha limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Senha limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Senha limite superior inválido', () => {throw 'Not implemented yet'});
+
+test('Senha inválida, sem números', () => {throw 'Not implemented yet'});
+
+test('Senha inválida, sem letras minúsculas', () => {throw 'Not implemented yet'});
+
+test('Senha inválida, sem letras maísculas', () => {throw 'Not implemented yet'});
+
+test('Senha inválida, com caractere especial', () => {throw 'Not implemented yet'});
+
+//####### CAMPO CONFIRMACAO SENHA ################################
+
+test('Confirmação de Senha inválida, diferente da senha anterior', () => {throw 'Not implemented yet'});
+
+test('Confirmação de Senha válida', () => {throw 'Not implemented yet'});
