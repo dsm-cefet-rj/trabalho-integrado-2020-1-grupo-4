c46c610a605c1edef41fb235b05ff3716a50dd90
Zirondi <zirondil@gmail.com>
2020-11-25 20:50:58 -0300

    ajustes pequenos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
-import Routes from "./Routes";
+import Routes from "./routes";
 import { getCurrentUserService } from "./store/auth/services";
 import { useDispatch } from "react-redux";
 import { AUTH_REDUCER_LOGOUT } from "./store/auth/reducer";
