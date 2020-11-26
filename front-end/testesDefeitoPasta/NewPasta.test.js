677432244f9acba0c2606f2ab1c0a6b4bb008723
Vitoria <vitoria10_@live.com>
2020-11-26 18:48:25 -0300

    Testes De Defeito Pasta - TERÃO INSTRUÇÕES!

---
+++ front-end/testesDefeitoPasta/NewPasta.test.js
@@ -0,0 +1,8 @@
+//Nova Pasta
+test('Nome Pasta limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Nome Pasta limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Nome Pasta limite superior inválido', () => {throw 'Not implemented yet'});
+
+test('Nome Pasta limite inferior inválido', () => {throw 'Not implemented yet'});
