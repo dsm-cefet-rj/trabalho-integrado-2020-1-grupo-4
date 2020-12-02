d5e321f4800c0f4f08f2782a6adf08b7af4baa14
Vitoria <vitoria10_@live.com>
2020-12-02 15:07:11 -0300

    conserto de Pasta.js para revisão

--- front-end/cypress/integration/Pasta/Pasta.js
+++ front-end/cypress/integration/Pasta/Pasta.js
@@ -27,13 +27,13 @@ Dado('que temos pastas cadastradas', async function (pastas) {
     }
 });
 
-Quando (`clicar no botão New Folder`, () => {
-    cy.get('#btn-new-folder').click(); //solicitar a id do botão New Folder
-});
+        Quando (`clicar no botão New Folder`, () => {
+            cy.get('#btn-new-folder').click(); //solicitar a id do botão New Folder
+        });
+        Entao(`A tela de pasta deve ser aberta, uma nova pasta foi criada`,() => {
+            cy.url().should('include', '/newPaste'); //solicitar a rota de Nova Pasta
+        });
 
-Entao(`A tela de pasta deve ser aberta, uma nova pasta foi criada`,() => {
-    cy.url().should('include', '/newPaste'); //solicitar a rota de Nova Pasta
-});
 
 Dado('esteja na tela de Arquivo', () => {
     cy.visit('/Archive',{
@@ -42,14 +42,16 @@ Dado('esteja na tela de Arquivo', () => {
         }
     }); //checar a rota correta de um arquivo existente
 });
+        Quando (`o usuário clicar em Deletar`, () => {
+            cy.get('#btn_deletar').click() //obter o id correto do botão deletar
+        });
+        Entao(`a aplicação retornará para "Pasta" e o arquivo será excluído `,() => {
+            cy.url().should('contain', '/')
+        });
+        E(`O arquivo excluído não deve mais estar nessa pasta`,() => {
+            cy.get('#1').should('Não existe')
+        });
 
-Quando (`o usuário clicar em Deletar`, (nome) => {
-    cy.get('#btn_deletar').click(); //obter o id correto do botão deletar
-});
-
-Entao(`a aplicação retornará para "Pasta" e o arquivo será excluído `,() => {
-    cy.url().should('contain', '/'); //Verificar a asserção de um arquivo excluído, garantir que ele foi excluído
-});
 
 Dado('esteja na tela da Pasta', () => {
 
@@ -57,66 +59,22 @@ Dado('esteja na tela da Pasta', () => {
         qs: {
             pasta: '1'
         }
-    }); //solicitar a rota de uma pasta existente, verificar se existem parametros na rota
+    }) //solicitar a rota de uma pasta existente, verificar se existem parametros na rota
         //aqui eu considerei como tendo parâmetros, pois existem varias pastas com seus ids
 });
-
-Quando (`o usuário clicar em Upload`, () => {
-    cy.get('#btn_upload').click(); //solicitar a id de Botão Upload.
-});
-
-Entao(`A tela de pasta deverá conter um novo arquivo`,() => {
-    cy.url().should('contain', '/'); //ver um jeito de saber como um novo arquivo estará lá
-});
-
-Quando (`clicar em uma pasta`, () => {
-    cy.get('#btn_pasta').click(); //obter um id correto para um botão de pasta
-});
-
-Entao(`a tela de pasta deve ser aberta`,() => {
-    cy.url().should('contain', '/folder');
-});
-
-Quando (`clicar em "New Folder"`, () => {
-    cy.get('#botao_11').click();
-});
+        Quando (`o usuário clicar em Upload`, () => {
+            cy.get('#btn_upload').click() //solicitar a id de Botão Upload.
+        });
+        Entao(`A tela de pasta deverá conter um novo arquivo`,() => {
+            cy.url().should('contain', '/') //ver um jeito de saber como um novo arquivo estará lá
+        });
 
 
-Dado('esteja navegando dentro de uma pasta', () =>{
-    //TODO
-});
-
-Quando('clicar no botão de "Upload"', () =>{
-    cy.get('#botao_33').click();
-});
-
-Entao('a tela de pasta deverá conter um novo arquivo', () => {
-    //TODO
-});
-
-Dado('tenha aberto um arquivo' , () =>{
-    //TODO
-});
-
-Quando('clicar em "Deletar"', () =>{
-    cy.get('#botao_35').click();
-});
-
-Dado('que é exibida a tela de pastas', () => {
-    cy.visit('/dashboard/folders');
-});
-
-
-Dado('que o usuário tenha entrado numa pasta', () => {
-    cy.visit('/yourarchive/1');
-});
-
-Quando('clico em um arquivo', () =>{
-    cy.get('#tabela_06 > .file').first().click(); //TODO pegar campo file certo quando for implementado
-});
-
-Entao('deve ser exibida a tela com as informações do arquivo', () => {
-    cy.url().should('include', '/file/1');
-    cy.get('#botao_34').should('be.visible'); // nao tem ID do nome do arquivo
-});
+        Quando('clico em um arquivo', () =>{
+            cy.get('#tabela_06 > .file').first().click() //TODO pegar campo file certo quando for implementado
+        });
+        Entao('deve ser exibida a tela com as informações do arquivo', () => {
+            cy.url().should('include', '/file/1')
+            cy.get('#botao_34').should('be.visible') // nao tem ID do nome do arquivo
+        });
 
